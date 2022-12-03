5ec5938d735e71f0a99b52ef9bc6ad8ae1e3aaf6
Pawel <paul.lutostanski99@gmail.com>
2022-12-03 11:18:03 +0100

    add one charge per bulb

--- Assets/ChargeStats.cs
+++ Assets/ChargeStats.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Rendering.Universal;
 
 [System.Serializable]
 public class ChargeEvent : UnityEvent<int> { }
@@ -9,6 +10,8 @@ public class ChargeStats : MonoBehaviour
 {
     [SerializeField]
     ChargeEvent _event;
+    [SerializeField]
+    Light2D chargeLight;
     int charge;
     int layerCharge;
     int layerAmplifier;
@@ -31,7 +34,10 @@ public class ChargeStats : MonoBehaviour
             if(collision.gameObject.TryGetComponent<ChargeStats>(out var otherChargeStats) && !otherChargeStats.isDisabled)
             {
                 otherChargeStats.isDisabled = true;
+                //(-((1)/(x))+1.2)*0.8
+                
                 charge += otherChargeStats.charge;
+                ChangeIntensity();
                 _event.Invoke(charge);
                 //transform.localScale = new Vector3(size, size, 0);
                 Destroy(collision.gameObject);
@@ -42,7 +48,14 @@ public class ChargeStats : MonoBehaviour
         {
  
             charge *= 2;
+            ChangeIntensity();
             _event.Invoke(charge);
         }
+    }
+
+    void ChangeIntensity()
+    {
+        if (charge > 0.85f)
+            chargeLight.intensity = (-((1f) / ((float)charge)) + 1.2f) * 0.8f;
     }
 }
